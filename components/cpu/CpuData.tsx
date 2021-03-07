59819b39a5e4bcde4e0884ac4bec5e6d0861720f
Sylvain Bataille <etu41216@henallux.be>
2021-03-07 18:51:09 +0100

    cpu chart

--- components/cpu/CpuData.tsx
+++ components/cpu/CpuData.tsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, TimerMixin } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
 import { Cpu, getCpuData } from '../../api/CpuApi';
+import { Dimensions } from "react-native";
+const screenWidth = Dimensions.get("window").width;
+
+const chartConfig = {
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`, // color of text
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
 
 interface IProps {
 }
@@ -8,32 +17,73 @@ interface IProps {
 interface IState {
     Cpu: Cpu
 }
+
+
 export class CpuData extends Component<IProps, IState> {
 
+    data = {
+        datasets: [
+            {
+                data: [0,0,0,0,0,0,0,0,0,0],
+                color: () => `rgba(134, 65, 244, 0.6)`, // color of data
+                strokeWidth: 3 // optional
+            },
+            {
+                data: [100,0],
+                color: (opacity = 0) => `rgba(0, 0, 0, 0)`, // color of data
+                strokeWidth: 0 // optional
+            }
+        ],
+        legend: ["CPU Usage"] // optional
+    };
+
+    refreshData = () => {
+        getCpuData().then((cpu) => {
+            let array = this.data.datasets[0].data
+            array.push(cpu.Usage);
+            if(array.length > 10){
+                array.splice(0, 1)
+            }
+            this.setState({
+                Cpu: cpu
+            })
+        })
+    }
     /**
      *
      */
     constructor(props: IProps) {
         super(props);
         this.state = {
-
+            Cpu: {
+                Temp: 0,
+                Usage: 0
+            }
         }
-        getCpuData().then((cpu) => {
-            console.log(cpu)
-            this.setState({
-                Cpu: cpu
-            })
-        })
+        this.refreshData();
+        setInterval(() => {
+            this.refreshData();
+        }, 5000)
     }
     render() {
-        console.log(this.state)
         if (!this.state.Cpu) {
             return (<View><Text>loading data</Text></View>)
         }
         return (
             <View>
-                <Text>Temp : {this.state.Cpu.Temp}</Text>
-                <Text>Usage : {this.state.Cpu.Usage}</Text>
+                <Text>Temp : {this.state.Cpu.Temp} C°</Text>
+                <LineChart
+                    data={this.data}
+                    width={screenWidth}
+                    height={220}
+                    chartConfig={chartConfig}
+                    fromZero={true}
+                    withDots={true}
+                    withInnerLines={false}
+                    segments={5}
+                    hidePointsAtIndex={[0,1,2,3,4,5,6,7,8,9]}
+                    withShadow={false}
+                />
             </View>
         )
     }
