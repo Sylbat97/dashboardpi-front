507c5d341f7d6b6e186c84011e1b7ad20266f2d3
Sylvain Bataille <etu41216@henallux.be>
2021-03-07 15:01:42 +0100

    header

--- App.tsx
+++ App.tsx
@@ -2,17 +2,37 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DiskDataList from './components/disk/DiskDataList';
-import { NavigationContainer } from '@react-navigation/native';
+import { DrawerActions, NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack'
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import { Icon } from 'react-native-elements';
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+export function DrawerNavigator() {
+  return (
+      <Drawer.Navigator initialRouteName="Disk">
+        <Drawer.Screen name="Disk" component={DiskDataList} />
+      </Drawer.Navigator>
+  );
+}
+
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Disk">
-        <Drawer.Screen name="Disk" component={DiskDataList} />
-      </Drawer.Navigator>
+      <Stack.Navigator initialRouteName="Disk"
+        screenOptions={({ navigation }) => ({
+          headerLeft: () => (
+            <TouchableHighlight onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}><Icon name='menu'></Icon></TouchableHighlight>),
+        })}>
+        <Stack.Screen
+          name="DashboardPi"
+          component={DrawerNavigator}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
