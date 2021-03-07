9aecba3f2d91eefe04f0ffb60667d9b3d0e21687
Sylvain Bataille <etu41216@henallux.be>
2021-03-07 13:42:03 +0100

    drawer nav

--- App.tsx
+++ App.tsx
@@ -2,12 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import DiskDataList from './components/disk/DiskDataList';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <DiskDataList />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Disk">
+        <Drawer.Screen name="Disk" component={DiskDataList} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
